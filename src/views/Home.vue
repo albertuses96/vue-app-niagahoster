c12d75c79428dd12db68ff2353f9865c247a6d33
dionaditya <dionjatingaleh@gmail.com>
2020-05-29 01:36:43 +0700

    :zap: remove uneccessary file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,9 +21,6 @@
 <script>
 // @ is an alias to /src
 import Button from '@/components/Button.vue'
-import EmployeeTable from '@/components/EmployeeTable.vue'
-import EmployeeForm from '@/components/EmployeeForm.vue'
-import JSON from '../../test.json'
 import AppBar from '@/components/AppBar'
 import MainContent from '@/components/MainContent.vue'
 import SecondaryContent from '@/components/SecondaryContent.vue'
@@ -43,8 +40,6 @@ export default {
   name: 'Home',
   components: {
     Button,
-    EmployeeTable,
-    EmployeeForm,
     AppBar,
     MainContent,
     SecondaryContent,
@@ -61,30 +56,10 @@ export default {
   computed: mapState({
     theme: state => state.theme
   }),
-  data() {
-    return {
-      employees: [
-        {
-          id: 1,
-          name: 'Richard Hendricks',
-          email: 'richard@piedpiper.com',
-        },
-        {
-          id: 2,
-          name: 'Bertram Gilfoyle',
-          email: 'gilfoyle@piedpiper.com',
-        },
-        {
-          id: 3,
-          name: 'Dinesh Chugtai',
-          email: 'dinesh@piedpiper.com',
-        },
-      ]
-    }
-  },
+
   async mounted() {
     this.$store.dispatch('getData')
-},
+  },
 
   methods: {
     ...mapActions([
